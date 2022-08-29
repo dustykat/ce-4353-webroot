38bff7ed5e0051a7042e407f6d79b9cd10c27a34
theodore cleveland <ted.cleveland@gmail.com>
2022-08-29 22:37:06 +0000

    update JB

--- ce4353jb/ce4353workbook/_build/jupyter_execute/lessons/computationaltools/topic1-2.py
+++ ce4353jb/ce4353workbook/_build/jupyter_execute/lessons/computationaltools/topic1-2.py
@@ -2,3 +2,45 @@
 # coding: utf-8
 
 # # Excel/Libre Office Spreadsheets
+# 
+# An alternative to developing scripts (computer programs) are spreadsheets.  Excel and LibreOffice are probably the most commonly available.  Excel because of Microsoft 360 subscription service no longer works very well for sharing working spreadsheets, but LibreOffice still works like the older Excel licensed packages.  Any spreadsheets in these notes are likely to be LibreOffice saved as `.xls`,`.xlsx`,or `.xlsm`  
+# 
+# ## Excel Spreadsheets
+# 
+# If you choose to do your work in Excel that's fine, but be sure to include in your work a rendering that shows formulas, something like
+# 
+# <figure align="center">
+# <img src="http://54.243.252.9/ce-4353-webroot/ce4353jb/ce4353workbook/lessons/computationaltools/excel-normal.png" width="400"> <figcaption>Figure 2.1. Excel spreadhseet "normal" appearance </figcaption>
+# </figure>
+# 
+# <!--![](excel-normal.png)-->
+# 
+# But **also** include a rendering with the formulas as
+# <figure align="center">
+# <img src="http://54.243.252.9/ce-4353-webroot/ce4353jb/ce4353workbook/lessons/computationaltools/excel-asformula.png" width="400"> <figcaption>Figure 2.2. Excel spreadhseet with "as formula" appearance </figcaption>
+# </figure>
+# 
+# <!--![](excel-asformula.png)-->
+# 
+# Furthermore, if you program macros, include the VBA code in an appendix.
+# 
+# ## LibreOffice Spreadsheets
+# 
+# These are nearly the same as Excel.  Here's the same two renderings in LibreOffice
+# 
+# <figure align="center">
+# <img src="http://54.243.252.9/ce-4353-webroot/ce4353jb/ce4353workbook/lessons/computationaltools/LibreOfficeNorm.png" width="400"> <figcaption>Figure 2.3. LibreOffice spreadhseet "normal" appearance </figcaption>
+# </figure>
+# 
+# <!--![](excel-normal.png)-->
+# 
+# But **also** include a rendering with the formulas as
+# <figure align="center">
+# <img src="http://54.243.252.9/ce-4353-webroot/ce4353jb/ce4353workbook/lessons/computationaltools/LibreOfficeAsF.png" width="400"> <figcaption>Figure 2.4. LibreOffice spreadhseet with "as formula" appearance </figcaption>
+# </figure>
+# 
+# <!--![](excel-asformula.png)-->
+# 
+# Much of the utility of Excel/LibreOffice and Jupyter is to generate graphical representations of calculations.  As such a review of graphing is useful.
+
+# ## References
--- ce4353jb/ce4353workbook/_build/jupyter_execute/lessons/computationaltools/topic1-3.py
+++ ce4353jb/ce4353workbook/_build/jupyter_execute/lessons/computationaltools/topic1-3.py
@@ -2,3 +2,7 @@
 # coding: utf-8
 
 # # HEC-RAS River Analysis Software
+# 
+# [HEC-River Analysis System (RAS)](https://www.hec.usace.army.mil/software/hec-ras/) the U.S. Army Corps of Engineers’ software package for modeling constant flows and flood-wave movement through a series of river cross sections, including the presence of a culvert structure. The software and documentation are available free of charge.
+# 
+# There are multiple versions.  Choose the version for your particular architecture (computer)
