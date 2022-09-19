5554c49f766948fe9852ddd63e366a93c32ff59b
theodore cleveland <ted.cleveland@gmail.com>
2022-09-19 00:57:59 +0000

    finish weir

---
+++ ce4353jb/ce4353workbook/_build/jupyter_execute/lessons/momentum/momentum3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Surges
+# 
+# What it is
+# 
+# ## Definitions
+# 
+# ## Examples
+# 
+# 
+# ## References
---
+++ ce4353jb/ce4353workbook/_build/jupyter_execute/lessons/momentum/momentum4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Bridge Piers
+# 
+# What it is
+# 
+# ## Definitions
+# 
+# ## Examples
+# 
+# 
+# ## References
