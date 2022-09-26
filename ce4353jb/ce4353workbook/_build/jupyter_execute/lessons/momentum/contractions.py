ec2d8e3d56a055d9677c973b7ffffffdd2677113
theodore cleveland <ted.cleveland@gmail.com>
2022-09-26 19:29:08 +0000

    update momentum

---
+++ ce4353jb/ce4353workbook/_build/jupyter_execute/lessons/momentum/contractions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Supercritical Contractions and Expansions
+# 
+# ## Contractions
+# 
+# ![](scontract1.png)
+# 
+# 
+# A photograph of such waves (in a laboratory) 
+# 
+# ![](contractOblique.png)
+# 
+# A more detailed sketch of the standing waves in plan view with various angles is analyzed using continunity ans momentum
+# 
+# ![](scontract2.png)
+# 
+# ![](scontract3.png)
+# 
+# I would speculate that in practice numerical models would be used to examine behavior and design contractions.  Something like [SToRM](https://i-ric.org/en/solvers/storm/) or [Telemac](http://www.opentelemac.org/) would be well suited to such a modeling task.
+# 
+# ## Expansions
+# 
+# ![](scontract4.png)
+# 
+# ![](scontract5.png)
+
+# ## Examples
+# 
+# 
+# ## References
+
+# 
---
+++ ce4353jb/ce4353workbook/_build/jupyter_execute/lessons/momentum/stillingbasins.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Stilling Basins
+# 
+# Used to create/fix a hydraulic jump in a known location to dissipate energy and prevent damage from super-duper-critical flow. 
+# 
+# ![](stilling1.png) 
+# 
+# 
+# 
+# The idea is to increase the apparent (to the water) channel roughness 
+# Momentum is used to deduce drag force imparted by the basin, and per-block projected area is combined into a total projected area for force approximation.
+# 
+# ![](stilling2.png) 
+# 
+# ![](stilling3.png)
+# 
+# The stilling basin itself includes the apron and the blocks and sill
+# 
+# ![](stilling4.png)
+# 
+# The USBR supercritical basins are depicted below:
+# 
+# ![](usbrsb1.png)
+# 
+# ![](usbrsb2.png)
+# 
+# ![](usbrsb3.png)
+# 
+# Here are some photographs (lifted from the mighty internet)
+# 
+# ![](sbphoto1.png)![](sbphoto2.png)
+# 
+# Physical models are used for design testing
+# 
+# ![](sbphoto3.png)
+# 
+# One in service
+# 
+# ![](sbphoto4.png)
+# 
+# Another one in service
+# 
+# ![](sbphoto5.png)
+# 
+# ## Examples
+# 
+# 
+# 
+# ## References
+# 
+# [Spillway Activating Various Dams (YouTube)](https://www.youtube.com/watch?v=4yuRXl3xgv0)
+
+# In[ ]:
+
+
+
+
