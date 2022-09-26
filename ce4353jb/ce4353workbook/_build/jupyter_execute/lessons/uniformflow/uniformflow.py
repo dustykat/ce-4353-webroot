32c0735f94669c00f79be126236e71460fbc9635
theodore cleveland <ted.cleveland@gmail.com>
2022-09-26 19:51:38 +0000

    update momentum

---
+++ ce4353jb/ce4353workbook/_build/jupyter_execute/lessons/uniformflow/uniformflow.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Uniform Flow
+# 
+# Uniform flow occurs when the average velocities in successive cross sections of a channel are the same. This occurs only when the cross section is constant. Non-uniform flow results from gradual or sudden changes in the cross sectional area.
+# If the water surface is parallel to the channel bottom, flow is uniform and the water surface is at normal depth $y_n$
+# 
+# Used for design of long open channels with the goal to have the water surface slope equal to the bed slope.  Heres a couple of pictures,
+# 
+# First a Central Aridzona Project canal
+# 
+# ![](CAPcanal.png)
+# 
+# And the Central Valley Project (Callyfornia) canal I grew up swimmin' in.
+# 
+# ![](CVPcanal.png)
+# 
+# The line of floaty balls means you are approaching one of these:
+# 
+# ![](CVPliftStation.png)
+# 
+# Even though swimmin' in the canals is illegal, the operators dont want to turn a bunch of kids into fish food, so they put up the balls.  
+# 
+# :::{note}
+# Despite the obvious Darwinian advantage of grinding up kids - dead meat does not pay taxes; support old folks; nor serve as a supply of harvestable kidneys.  Hence we try to save them.
+# :::
+
+# 
