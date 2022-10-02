63c85d5d06a9cf719e4db8be0db2bc2bfa8bec5b
theodore cleveland <ted.cleveland@gmail.com>
2022-10-02 21:43:05 +0000

    update uniform

---
+++ ce4353jb/ce4353workbook/_build/jupyter_execute/lessons/uniformflow/gutters.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Gutter Flow and Gravity Sewer Design
+# 
+# ## Gutter Geometry
+# 
+# ## Inlets
+# 
+# ## Gravity Sewers
+# 
+# ## Examples
+# 
+
+# In[24]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
