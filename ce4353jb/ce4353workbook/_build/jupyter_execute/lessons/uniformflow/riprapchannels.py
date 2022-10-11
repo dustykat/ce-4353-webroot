6d29a9edb7df041edbcc681660b9a098f1e07529
theodore cleveland <ted.cleveland@gmail.com>
2022-10-11 14:51:44 +0000

    update uniformflow

---
+++ ce4353jb/ce4353workbook/_build/jupyter_execute/lessons/uniformflow/riprapchannels.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Rip-Rap Lined Channels
+# 
+# Rip-rap lined channels refers to engineered channels with a flexible lining in distinct contrast with a rigid lined channel (i.e. concrete).  
+# 
+# :::{note}
+# Riprap (in North American English), also known as rip rap, rip-rap, shot rock, rock armour (in British English) or rubble, is human-placed rock or other material used to protect shoreline structures against scour and water, wave, or ice erosion. Ripraps are used to armor shorelines, streambeds, bridge abutments, foundational infrastructure supports and other shoreline structures against erosion.
+# :::
+# 
+# The actual materials vary for example stone rip-rap in Texas applications can be found at [TxDOT Rip-Rap Supplier Web Page](https://www.gcli.com/product-page/txdot-protection-rip-rap)
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# Trapezoidal lined channel 1.5 meters bottom width, 3:1 side slope, $S_0$ is 0.012 (1.2 percent). Grass-legume mixture with mature growth height of 15-20 cm.  What is maximum allowable discharge for this lining (without detaching the lining)?
