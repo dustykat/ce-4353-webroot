24c77b296312fd71eb8e6a05b60fa8aca7db5e06
theodore cleveland <ted.cleveland@gmail.com>
2022-11-30 20:34:30 +0000

    hydprofiles

---
+++ ce4353jb/ce4353workbook/_build/jupyter_execute/lessons/hydraulicprofiles/hydraulicprofiles.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Hydraulic Profiles through Treatment Plants
+# 
+# ## Head Loss by Components
+# 
+# ### Closed Conduits
+# 
+# - Pipes
+# - Valves
+# - Bends
+# - Pumps (negative head loss)
+# 
+# ### Open Conduits
+# 
+# - Channels between reactors
+# - Reactors (as long basins)
+# - Disinfection contact reactors (i.e. chlorine)
+# - Discharge weirs
+# 
+# ### Unit Processes
+# 
+# - Bar Screens, Grit Chambers, Hydrocyclones : 0.5 feet - 35\% flow depth
+# - Clarifiers:  can estimate from outlet structure type (orifice, weir)
+# - Rapid-mix, Slow-mix (floculator), Aerated reactors: can estimate from outlet structure type (orifice, weir)
+# - Filters: 
+# 
+# ## References
+# 
+# 1. [Hydraulic Profiles in Wastewater Plants](http://54.243.252.9/ce-4353-webroot/ce4353jb/ce4353workbook/lessons/hydraulicprofiles/HydraulicProfilesWastewaterPlants.pdf)
+# 2. [WWTP Hydraulic Modeling (Cincinnatti)](http://www.ohiowea.org/docs/Th1000Potp_Hydraulic_Model.pdf)
+# 
+
+# In[ ]:
+
+
+
+
